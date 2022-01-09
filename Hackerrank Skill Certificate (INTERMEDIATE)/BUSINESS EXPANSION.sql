99d7696855cc71d6e645f34bcdbd33aef30611f8
Gaurav Topre <gauravtopre9@gmail.com>
2022-01-09 12:11:57 +0530

    Add files via upload

---
+++ Hackerrank Skill Certificate (INTERMEDIATE)/BUSINESS EXPANSION.sql
@@ -0,0 +1,7 @@
+#BUSINESS EXPANSION
+
+select ua.id,ua.first_name,ua.last_name, cu.id,cu.customer_name,count(cu.id) 
+from customer cu, user_account ua, contact c
+where cu.id = c.customer_id and c.user_account_id = ua.id 
+group by ua.id, ua.first_name, ua.last_name,cu.id,cu.customer_name 
+having count(cu.id)>1
